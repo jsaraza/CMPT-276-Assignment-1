e211a59c59d3c76bf3a66c2e0f3b830877085fd6
Jaime Saraza <70734810+jsaraza@users.noreply.github.com>
2024-05-30 20:38:05 -0700

    first commit

---
+++ src/main/java/cmpt276/gradingcalculator/GradingcalculatorApplication.java
@@ -0,0 +1,13 @@
+package cmpt276.gradingcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmpt276/gradingcalculator/GradingcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpt276.gradingcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
